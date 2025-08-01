a90bc600a58a4e704457acb9c82b20e1ddadade9
H51agarwal <hans15dec@gmail.com>
2025-08-01 14:37:41 +0530

    dark mode styling

--- BudgetBuddy.js
+++ BudgetBuddy.js
@@ -1,3 +1,23 @@
+// Sign up form 
+
 document.getElementById("SignUp_03").addEventListener("click", function () {
     window.location.href = "signUp.html";
+});
+
+// Light-dark mode switching
+
+const themeToggle = document.getElementById("themeToggle");
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    themeToggle.textContent = "☀️";
+}
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+    } else {
+        themeToggle.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    }
 });
